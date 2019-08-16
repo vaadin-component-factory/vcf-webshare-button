e91abc3e10b7e0a63175aca988daad5cdba2f5e7
Nii Yeboah <nii@vaadin.com>
2019-08-16 12:51:47 +0300

    Add webshare button code

--- src/vcf-webshare-button.js
+++ src/vcf-webshare-button.js
@@ -1,6 +1,9 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
 import { ElementMixin } from '@vaadin/vaadin-element-mixin';
+import '@vaadin/vaadin-button';
+import '@polymer/iron-icon';
+import './icons';
 
 class VcfWebshareButton extends ElementMixin(ThemableMixin(PolymerElement)) {
   static get template() {
@@ -9,7 +12,20 @@ class VcfWebshareButton extends ElementMixin(ThemableMixin(PolymerElement)) {
         :host {
           display: block;
         }
+        :host([hidden]) {
+          display: none !important;
+        }
       </style>
+      <vaadin-button
+        id="button"
+        theme="tertiary small"
+        on-click="openWebShare"
+        title="[[shareButtonText]]"
+        hidden$="[[!browserCompatible]]"
+      >
+        <iron-icon icon="taskmob:share" slot="prefix"></iron-icon>
+        <span>[[shareButtonText]]</span>
+      </vaadin-button>
     `;
   }
 
@@ -22,7 +38,59 @@ class VcfWebshareButton extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get properties() {
-    return {};
+    return {
+      shareUrl: String,
+      shareTitle: {
+        type: String,
+        value: document.title
+      },
+      shareText: {
+        type: String,
+        value: ''
+      },
+      shareButtonText: {
+        type: String,
+        value: 'Share...'
+      },
+      browserCompatible: {
+        type: Boolean,
+        value: 'share' in navigator
+      }
+    };
+  }
+
+  ready() {
+    super.ready();
+    this.shareUrl = this.shareUrl || this.getDefaultUrl();
+  }
+
+  openWebShare() {
+    if (window.ga) {
+      window.ga('send', 'event', 'Sharing', 'native api used');
+    }
+    navigator
+      .share({
+        title: this.shareTitle,
+        text: this.shareText,
+        url: this.shareUrl
+      })
+      .catch(console.error);
+  }
+
+  getDefaultUrl() {
+    let url = window.location.href;
+    const canonical = document.querySelector('link[rel=canonical]');
+    if (canonical) {
+      url = canonical.href;
+    }
+    return url;
+  }
+
+  /**
+   * @protected
+   */
+  get focusElement() {
+    return this.$.button;
   }
 }
 
