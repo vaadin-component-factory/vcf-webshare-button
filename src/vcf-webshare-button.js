7c7f0988d67fff41e1c4f68f9bb048df2cb94a9f
Nii Yeboah <nii@vaadin.com>
2019-08-16 17:10:23 +0300

    Update vaadin-button dep

--- src/vcf-webshare-button.js
+++ src/vcf-webshare-button.js
@@ -34,7 +34,7 @@ class VcfWebshareButton extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '0.1.0';
+    return '0.1.1';
   }
 
   static get properties() {
